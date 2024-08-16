f8f1be47e064ae6279a97edd3a72b1ca103f5ab3
Bamzhie <sheuahmad@gmail.com>
2024-08-16 12:17:32 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+// tailwind.config.js
+module.exports = {
+  theme: {
+    extend: {
+      screens: {
+        sm: "24rem",
+        md: "48rem",
+        lg: "56rem",
+        xl: "64rem",
+        ll: "80rem",
+        mm: "90rem",
+        nn: "102rem",
+        tp: "128rem",
+        op: "160rem",
+      },
+    },
+  },
+  plugins: [],
+};
